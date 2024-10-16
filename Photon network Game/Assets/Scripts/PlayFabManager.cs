baabc2a9da41f81341f1e14069243aa39cbb776b
Dalikun4208 <ldk78423@gmail.com>
2024-10-16 19:42:50 +0900

    20241016

---
+++ Photon network Game/Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+        PhotonNetwork.GameVersion = "1.0f";
+        PhotonNetwork.LoadLevel("LobbyScene");
+    }
+
+    public void Success(RegisterPlayFabUserResult registerPlayFabUserResult)
+    {
+        Debug.Log(registerPlayFabUserResult.Request);
+        Debug.Log(registerPlayFabUserResult.ToString());
+
+    }
+
+    public void Failure(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+
+    public void OnSignIn()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
+
+    }
+
+    public void OnSignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            Success,
+            Failure
+        );
+    }
+
+}
